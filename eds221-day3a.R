b12cd12d31b3ad25e8d0df6362129a9f9f4af208
ht-oliver <holiver327@gmail.com>
2025-08-13 14:31:11 -0700

    oh god

--- eds221-day3a.R
+++ eds221-day3a.R
@@ -56,6 +56,142 @@ species <- "mouse"
 
 switch(species,
        "cat" = print("meow"), #same as writing a if statement
-       "mouse" = print ("squeak"),
-       "dog" = print ("woof")
+       "mouse" = print ("squeak"), #same as else if
+       "dog" = print ("woof"), #same as final else
        )
+
+# Next week we'll do dplyr::case_when()
+
+#Writing for loops
+
+duck_names <- c("Billy", "Waddle", "Fluffy", "Dr. Duck")
+print(paste("The duck that got water in the living room is", duck_names[1]))
+print(paste("The duck that got water in the living room is", duck_names[2]))
+print(paste("The duck that got water in the living room is", duck_names[3]))
+print(paste("The duck that got water in the living room is", duck_names[4]))
+
+# version with a variable that I can update
+quackler <- duck_names[1]
+
+print(paste("The duck that got water in the living room is", duck_names[quackler]))
+
+# convert to a for loop
+for (quackler in duck_names) {
+  print(paste("The duck that got water in the living room is", quackler))
+}
+
+
+# Another loop example
+mass <- seq(from = 0, to = 3, b =0.5)
+
+i <- mass[1]
+i
+new_val <- i + 2 #adding 2 to each value
+print(new_val) #print new values
+
+# Write into a for loop
+for (i in mass) {
+  new_val <- i + 2 
+  print(new_val)
+}
+
+
+# PRactice same thing but with indexing
+i <- 1
+new_val <- mass[i] + 2
+print(new_val)
+
+
+# Make it a for loop
+
+for (i in 1:length(mass)) { # defining iterator using seq_along
+  i <- 1
+  new_val <- mass[i] + 2
+  print(new_val)
+}
+
+for (i in seq_along(mass)) { # defining iterator using seq_along
+  i <- 1
+  new_val <- mass[i] + 2
+  print(new_val)
+}
+
+
+#God damn I'm hungry
+tree_height <- c( 1, 2, 6, 10)
+
+tree_height[1] + tree_height[2]
+
+# convert into a generalizable expression
+i <- 1
+val <- tree_height[i] + tree_height[i+1]
+print(val)
+
+# convert into a for loop
+# test out creating the sequence
+seq_along(tree_height)
+
+for (i in seq_along(tree_height)) {
+  val <- tree_height[i] + tree_height[i + 1]
+  print(val)
+}
+
+# For loops with conditional statements
+
+# create a vector of things
+
+fruit <- c("apple", "clementine", "banana", "banana", "grapes")
+
+for ( i in seq_along(fruit)) {
+  if (fruit[i] == "banana") {
+    print("I love bananas")
+  } else {
+    print("These are other fruit")
+  }
+}
+
+# Another for loop example with conditional statements
+
+# fruit species
+species <- c("dog", "elephant", "goat", "dog", "dog", "elephant")
+
+# and their respective ages in humans years
+age_human <- c(3, 8, 4, 6, 12, 18)
+
+# convert ages to "animals years" using the following:
+# 1 human years = 7 dogs years
+# 1 human year = 0.88 in elephant years
+# 1 human year = 4.7 goat years
+
+# allocate space for our output
+animal_ages <- vector(mode = "numeric", length = length(species))
+
+
+for (i in seq_along(species)){
+if (species[i] == "dog") {
+  animal_age <- age_human[i] * 7
+} else if (species [i] == "elephant") {
+  animal_age <- age_human[i] * 0.88
+} else if (species[i] == "goat") {
+  animal_age <- age_human[i] * 4.7
+}
+  animal_ages[i] <- animal_age #populate empty animal ages vector
+}
+
+animal_age
+
+# Another example of storing an output
+
+tigers <- c(29, 34, 82)
+lions <- c(2, 18, 6)
+
+
+big_cats <- vector(mode = "numeric", length = length(tigers))
+
+for (i in seq_along(tigers)) {
+  total_cats <- tigers[i] + lions[i]
+  big_cats[i] <- total_cats}
+big_cats
+
+
+
